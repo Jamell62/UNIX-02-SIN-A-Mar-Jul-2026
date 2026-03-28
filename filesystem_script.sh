01cc31f04cdb36fa96f43e67427b5d830ecc6e55
Jamell62 <odalissevillano1712@gmail.com>
2026-03-28 04:31:31 +0000

    hello.c

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,3 +1,5 @@
 cd /dev #Changes the current directory to /dev, which contains system device files
 ls #Lists files and directories in the current directory
 history #Shows previously executed commands in the terminal
+cd /proc
+cat cpuinfo
