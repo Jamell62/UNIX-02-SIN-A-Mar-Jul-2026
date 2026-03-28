1d30ecdd45b08c2cd786585c5564e91be311f32a
Jamell62 <odalissevillano1712@gmail.com>
2026-03-28 01:44:07 +0000

    doc_cifrado

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -7,3 +7,6 @@ gpg --list-secret-keys --keyid-format=long #Show private keys
 gpg --armor --export-secret-keys odalissevillano1712@gmail.com #Export private key
 gpg --list-keys #Shows all public keys stored in your GPG keyring
 gpg --import MICOMPA_llave_publica.asc #Import public key
+echo "este mensaje es secreto" > doc_no_cifrado.txt
+ls #Lists files and directories in the current folder.
+cat doc_no_cifrado.txt #Displays the content of the file on the terminal.
