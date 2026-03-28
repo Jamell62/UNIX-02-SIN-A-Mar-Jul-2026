c15300a5fa9ef64bcf5c0faabb1cca9091553c0e
Jamell62 <odalissevillano1712@gmail.com>
2026-03-28 01:00:12 +0000

    my key public

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,6 @@ uname -a #daba información del ordenador
 which gpg # ubicacióin del binario de gpg
 gpg --version # me da la version e informacion adicional
 gpg --full-key-generate #genero la llave publica y privada
+gpg -list-keys
+gpg --list-secret-keys --keyid-format=long
+gpg --armor --export-secret-keys odalissevillano1712@gmail.com
