99bc9075c97bfb5a5aeb1ec96bc0fa95f59d1bdc
Jamell62 <odalissevillano1712@gmail.com>
2026-03-28 04:23:28 +0000

    Change directory

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,4 +1,3 @@
-# change directory to /dev directory
-cd /dev
-# list files and directories on the /dev directory
-ls
+cd /dev #Changes the current directory to /dev, which contains system device files
+ls #Lists files and directories in the current directory
+history #Shows previously executed commands in the terminal
