36191e213b31856a6d97946d060aadc13554d993
Jamell62 <odalissevillano1712@gmail.com>
2026-03-28 01:14:03 +0000

    Import Paula´s key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,7 +1,9 @@
-uname -a #daba información del ordenador
-which gpg # ubicacióin del binario de gpg
-gpg --version # me da la version e informacion adicional
-gpg --full-key-generate #genero la llave publica y privada
-gpg -list-keys
-gpg --list-secret-keys --keyid-format=long
-gpg --armor --export-secret-keys odalissevillano1712@gmail.com
+uname -a #Shows system information about your computer.
+which gpg #Shows the location of the gpg binary.
+gpg --version # Shows: GPG version algorithmsinformation
+gpg --full-key-generate #Generate keys
+gpg -list-keys #Show public keys
+gpg --list-secret-keys --keyid-format=long #Show private keys
+gpg --armor --export-secret-keys odalissevillano1712@gmail.com #Export private key
+gpg --list-keys #Shows all public keys stored in your GPG keyring
+gpg --import MICOMPA_llave_publica.asc #Import public key
