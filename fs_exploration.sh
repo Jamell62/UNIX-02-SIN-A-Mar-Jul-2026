82198f1544cdc01508eaa5158ec9ff20a5340985
Jamell62 <odalissevillano1712@gmail.com>
2026-04-02 12:51:07 +0000

    Install parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,4 +1,14 @@
 #changes the current director
 cd /
+
 #lists files adding symbols to show their types
 ls -F
+
+#Run the APT package manager as superuser to refresh the list of available packages
+sudo apt update
+
+#Run the APT package manager as superuser to upgrade all installed packages to their latest versions
+sudo apt upgrade
+
+#Run the APT package manager as superuser to install the program called parted.
+sudo apt install parted
