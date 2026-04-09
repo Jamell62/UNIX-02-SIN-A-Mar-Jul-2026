66018648e4db39b3461a318146224cdf15482ce4
Jamell62 <odalissevillano1712@gmail.com>
2026-04-09 12:26:48 +0000

    Creation of a new folder

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+#This command creates a new directory called “proyecto” in your current location. It’s basically the place where you will store all the files for your project.
+mkdir proyecto 
+
+#Here you are changing directories using an absolute path. That means you are specifying the full location of the folder starting from the root (/).
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
+
+#This command uses a relative path, which means it depends on your current location.
+cd proyecto/
