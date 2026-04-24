7bec7e056ce5c0a4c0021a6a8a8502d34bea5d9b
Jamell62 <odalissevillano1712@gmail.com>
2026-04-24 16:39:17 +0000

    Fix problema 2: archivos movidos y renombrados

---
+++ evaluation/history.sh
@@ -0,0 +1,13 @@
+problema 1
+   14  cd evaluation
+   15  find kepler/ -ls
+   16  ls kepler/
+   17  ls kepler/registros/
+   18  mv kepler/docs kepler/registros
+   19  mkdir kepler/plantillas
+   20  ls
+   21  ls kepler/
+   22  git add .
+   23  git commit -m "Fix problema 1: estructura de directorios corregida"
+
+problema 2
