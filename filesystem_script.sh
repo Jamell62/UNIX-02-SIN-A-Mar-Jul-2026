cf5c3e5fff833bba2f065113866a8f98d5cb6798
Jamell62 <odalissevillano1712@gmail.com>
2026-05-10 15:17:58 +0000

    information change

--- filesystem_script.sh
+++
@@ -1,5 +0,0 @@
-cd /dev #Changes the current directory to /dev, which contains system device files
-ls #Lists files and directories in the current directory
-history #Shows previously executed commands in the terminal
-cd /proc
-cat cpuinfo
