c59a59fb6c8e9e6b286ca670619f24e63b947fdb
Jamell62 <odalissevillano1712@gmail.com>
2026-06-04 13:03:42 +0000

    Debugging

--- blackhatbash.sh
+++ blackhatbash.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -x
 bash --version
 env
 echo ${SHELL}
@@ -9,5 +10,6 @@ man ls #Display the manual page for a comman
 ps -e -f
 ps -ef
 df -h
-bash -r blackhatbash.sh
-bash -n blackhatbash.sh
+bash -r blackhatbash.sh # Run a Bash script in restricted mode
+bash -n blackhatbash.sh # Check a Bash script for syntax errors without executing it
+set +x
