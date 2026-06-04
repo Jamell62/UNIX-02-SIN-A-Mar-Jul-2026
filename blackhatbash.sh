ae41aa6dd5b7dc25770c35a6dd199cd31653437c
Jamell62 <odalissevillano1712@gmail.com>
2026-06-04 12:56:51 +0000

    Display system information, process details, disk usage, and check Bash script execution or syntax

--- blackhatbash.sh
+++ blackhatbash.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+man ls #Display the manual page for a comman
+ps -e -f
+ps -ef
+df -h
+bash -r blackhatbash.sh
+bash -n blackhatbash.sh
