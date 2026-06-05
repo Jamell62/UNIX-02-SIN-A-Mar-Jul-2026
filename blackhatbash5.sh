5eeb3ca2acd54220c618fc3e34f23d1c5c894f26
Jamell62 <odalissevillano1712@gmail.com>
2026-06-05 17:17:40 +0000

    Demonstrates Bash command execution, file creation and modification, appending text, viewing file contents, redirecting standard output and standard error to files (separately or together), and capturing errors from failed commands.

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -5,3 +5,15 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "the lzl command failed"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye Bye!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+lzl 2> error.txt
+cat error.txt
