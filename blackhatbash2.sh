d8f7749dd2c1088908eb1486220870b41698bf66
Jamell62 <odalissevillano1712@gmail.com>
2026-06-07 15:41:42 +0000

    Delete files that belong to another branch

--- blackhatbash2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# All this script does is create a directory, create a file # within the directory, and then list the contents of the directory. 
-mkdir blackhatbash2.sh
-touch blackhatbash2.sh/myfile.txt 
-set -x
-ls -l blackhatbash2.sh
-set +x
