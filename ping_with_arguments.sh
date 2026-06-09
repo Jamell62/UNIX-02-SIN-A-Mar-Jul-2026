46339bca51fbb05cdb613a3b61df353b2ed36274
Jamell62 <odalissevillano1712@gmail.com>
2026-06-09 16:55:59 +0000

    # Prompts the user for their first and last name, then displays them back.

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -5,3 +5,18 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..." 
 echo "Pinging the target: ${TARGET}..." 
 ping "${TARGET}"
+
+echo "The arguments are: $*@" 
+echo "The total number of arguments is: $#"
+
+# Change "$@" to "$*" to observe behavior.
+for args in "$@"; do
+   echo "${args}"
+done
+
+echo "What is your first name?" 
+read -r firstname 
+echo "What is your last name?" 
+read -r lastname 
+echo "Your first name is ${firstname} and your last name is 
+${lastname}"
