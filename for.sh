6df601d574b5cb040885db7333322213be0705d2
Jamell62 <odalissevillano1712@gmail.com>
2026-06-12 17:18:05 +0000

    .

--- for.sh
+++ for.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Specifies that the script should be executed using the Bash shell.
-for i in {1..5}; do
-    echo "Number: $i"
+
+for index in $(seq 1 10); do
+    echo "${index}"
 done
