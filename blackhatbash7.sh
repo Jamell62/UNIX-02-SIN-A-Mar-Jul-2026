4761acadafaf239aab5410fbafc25934538f3e9a
Jamell62 <odalissevillano1712@gmail.com>
2026-06-18 13:16:00 +0000

    BYE

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+grep "35.237.4.214" log.txt
+grep "35.237.4.214\|13.66.139.0" log.txt #backslash signifies scape character, can be replaced by -e
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
 ps | grep TTY
 ps | grep -i tty 
 grep -v "35.237.4.214" log.txt
