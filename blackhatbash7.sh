f3f60d8a90e4eb6faf6271e064a42a9f1b42649f
Jamell62 <odalissevillano1712@gmail.com>
2026-06-18 13:10:18 +0000

    View file content, extract fields with awk, and filter/search specific IP entries using grep

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -6,3 +6,9 @@ grep -o "35.237.4.214" log.txt
 awk '{print $1}' log.txt
 cat log.txt 
 awk '{print $1,$2,$3}' log.txt
+awk '{print $1,$NF}' log.txt
+awk -F',' '{print $1}' example_file2
+head log.txt
+grep "42.236.10.117" log.txt
+awk '{print $7}' log.txt
+grep "42.236.10.117" log.txt | awk '{print $7}'
