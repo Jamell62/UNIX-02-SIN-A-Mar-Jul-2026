cb1b371f70a156e0b6e7adff8e125fce9a236c00
Jamell62 <odalissevillano1712@gmail.com>
2026-06-18 13:16:32 +0000

    .

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -10,7 +10,7 @@ awk '{print $1}' log.txt
 cat log.txt 
 awk '{print $1,$2,$3}' log.txt
 awk '{print $1,$NF}' log.txt
-awk -F',' '{print $1}' example_file2
+awk -F',' '{print $1}' example_csv.txt.
 head log.txt
 grep "42.236.10.117" log.txt
 awk '{print $7}' log.txt
